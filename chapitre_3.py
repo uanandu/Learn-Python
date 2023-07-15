6ff03dfbe918c0831b8d834f55cd1bd02cb24d97
uanandu <ananduun@icloud.com>
2023-07-15 19:33:41 +0000

    some more exercises for Ch.3

--- chapitre_3.py
+++ chapitre_3.py
@@ -1,3 +1,4 @@
+
 # Lists
 
 # 3-1: Names
@@ -48,3 +49,30 @@ mode_of_transportation = ["car", "Harley", "Bicycle"]
 
 for item in mode_of_transportation:
     print(f"I would like to own a {item}!")
+
+# 3-4 : Guest list
+message = "We cordially invite you to  our function on 27th August 2023:"
+guest_list = ["Asoka", "Chola", "Buddha", "Krishna", "Ram", "Jesus", "Anandu", "Athul"]
+for guest in guest_list:
+	print(message, f"{guest}")
+
+# 3-5: Changing the guest list
+print("After getting the RVSP response we see that some of the guests aren't gonna make it")
+guest_list.remove("Asoka")
+guest_list.insert(6, "Darwin")
+for guest in guest_list:
+	print(message, f"{guest}")
+
+print("#################")
+print("Two days later!!")
+print("#################")
+
+print("Hey guys,i've found a bigger table for you guys. I would love to transfer you to that and add some more members!!!!"
+
+guest_list.insert(1, "Freud")
+guest_list.insert(5, "Anne")
+guest_list.append("Andrea")
+
+for guest in guest_list:
+	print(f"Hey {guest}, please do show up at the place given in the invitation")
+
