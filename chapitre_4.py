9398c163c0100d61121bd4e1e2b433f4783cb837
uanandu <ananduun@icloud.com>
2023-07-16 16:22:48 +0000

    added cha[ter 4

---
+++ chapitre_4.py
@@ -0,0 +1,20 @@
+# Working with lists
+
+# 4-1: Pizzas
+pizzas = ["All-dressed", "Chicken-pesto", "Boston", "Alfredo-chicken"]
+
+for pizza in pizzas:
+	print(f"I love {pizza} pizza!")
+
+print("I really love pizza!!!!")
+
+# 4-2:Animals
+
+animals= ["cat", "dog", "goat", "chicken", "hamster"]
+
+for animal in animals:
+	print(f"A {animal} is a great pet")
+
+print("All these animals have legs in common")
+
+
